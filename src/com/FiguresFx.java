3572617451bb3443828546f88822758867348e4f
Andreu <a.pialenko@gmail.com>
2019-02-01 19:35:05 +0300

    add

--- src/com/FiguresFx.java
+++ src/com/FiguresFx.java
@@ -5,8 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.log4j.Logger;
 
 public class FiguresFx extends Application{
+    private static final Logger LOGGER = Logger.getLogger(FiguresFx.class);
+
     @Override
     public void start(Stage window) throws Exception {
         window.setTitle("FiguresFx");
@@ -17,6 +20,8 @@ public class FiguresFx extends Application{
     }
 
     public static void main(String[] args) {
+        LOGGER.info("Your JavaFx App started");
         launch();
+
     }
 }
